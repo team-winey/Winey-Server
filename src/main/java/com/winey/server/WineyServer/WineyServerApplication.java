e32560fe9652cd82c88aaef29d8900c7c0409932
sunny <rosa7536@naver.com>
2023-06-28 17:24:45 +0900

    [Init] 프로젝트 기본 세팅

---
+++ src/main/java/com/winey/server/WineyServer/WineyServerApplication.java
@@ -0,0 +1,13 @@
+package com.winey.server.WineyServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineyServerApplication.class, args);
+	}
+
+}
