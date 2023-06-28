bea694ecb24730fbf80a8f73b759a16dfec2ab0d
sunny <rosa7536@naver.com>
2023-06-29 00:51:08 +0900

    [REFACTOR] 프로젝트 구조 변경

--- src/main/java/org/winey/server/ServerApplication.java
+++ src/main/java/org/winey/server/ServerApplication.java
@@ -1,13 +1,13 @@
-package com.winey.server.WineyServer;
+package org.winey.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WineyServerApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WineyServerApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
