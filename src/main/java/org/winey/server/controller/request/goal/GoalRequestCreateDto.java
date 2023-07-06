a201928b374cc56711fb991a44f72e87978f9e11
sunny <rosa7536@naver.com>
2023-07-06 16:34:24 +0900

    #9 [Chore] 파일 생성

---
+++ src/main/java/org/winey/server/controller/request/goal/GoalRequestCreateDto.java
@@ -0,0 +1,13 @@
+package org.winey.server.controller.request.goal;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class GoalRequestCreateDto {
+    
+}
---
+++ src/main/java/org/winey/server/service/GoalService.java
@@ -0,0 +1,13 @@
+package org.winey.server.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GoalService {
+
+    @Transactional
+    public
+}
