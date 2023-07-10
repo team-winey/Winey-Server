52ef69a1ce48bb2c3cca8aef2112003a5c783b9f
Sunny <rosa7536@naver.com>
2023-07-10 11:12:26 +0900

    #14 [Feat] repository

---
+++ src/main/java/org/winey/server/controller/UserController.java
@@ -0,0 +1,22 @@
+package org.winey.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.winey.server.common.dto.ApiResponse;
+import org.winey.server.controller.response.user.UserResponseDto;
+import org.winey.server.exception.Success;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<UserResponseDto> getUser(@RequestHeader Long userId) {
+        return ApiResponse.success(Success.GET_USER_SUCCESS, userService.)
+    }
+}
--- src/main/java/org/winey/server/infrastructure/GoalRepository.java
+++ src/main/java/org/winey/server/infrastructure/GoalRepository.java
@@ -1,14 +1,19 @@
 package org.winey.server.infrastructure;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.winey.server.domain.goal.Goal;
+import org.winey.server.domain.user.User;
+
+import java.util.List;
 
 public interface GoalRepository extends Repository<Goal, Long> {
     // CREATE
     Goal save(Goal goal);
 
     // READ
-
+    @Query("select g from Goal g where g.user = ?1 order by g.createdAt DESC")
+    List<Goal> findByUserOrderByCreatedAtDesc(User user);
     // UPDATE
 
     // DELETE
