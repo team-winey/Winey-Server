2d456409da52f0f2807cec1ff0d065705659759f
Sunny <rosa7536@naver.com>
2023-07-10 14:44:40 +0900

    #14 [Feat] User Service

--- src/main/java/org/winey/server/service/UserService.java
+++ src/main/java/org/winey/server/service/UserService.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.winey.server.controller.response.user.UserResponseDto;
+import org.winey.server.controller.response.user.UserResponseGoalDto;
+import org.winey.server.controller.response.user.UserResponseUserDto;
+import org.winey.server.domain.goal.Goal;
 import org.winey.server.domain.user.User;
 import org.winey.server.exception.Error;
 import org.winey.server.exception.model.NotFoundException;
 import org.winey.server.infrastructure.GoalRepository;
 import org.winey.server.infrastructure.UserRepository;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +28,19 @@ public class UserService {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
 
+        UserResponseUserDto userDto = UserResponseUserDto.of(user.getUserId(), user.getNickname(), user.getUserLevel().getName());
 
+        List<Goal> goalList = goalRepository.findByUserOrderByCreatedAtDesc(user);
 
+        if (goalList.size() == 0) {
+            return UserResponseDto.of(userDto, null);
+        }
+        Goal presentGoal = goalList.stream().findFirst()
+                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_GOAL_EXCEPTION, Error.NOT_FOUND_GOAL_EXCEPTION.getMessage()));
+
+        int targetDay = (int) Duration.between(presentGoal.getTargetDate(), presentGoal.getCreatedAt().toLocalDate()).toDays();
+        UserResponseGoalDto goalDto = UserResponseGoalDto.of(presentGoal.getDuringGoalAmount(), presentGoal.getDuringGoalCount(), presentGoal.getTargetMoney(), targetDay, LocalDate.now().isAfter(presentGoal.getTargetDate()), presentGoal.isAttained());
+
+        return UserResponseDto.of(userDto, goalDto);
     }
 }
