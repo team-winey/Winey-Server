c059799adaf4fedb5789bff156031a9d6ea5e3ad
Sunny <rosa7536@naver.com>
2023-07-10 13:22:17 +0900

    #14 [Refactor] dto 수정

--- src/main/java/org/winey/server/controller/response/user/UserResponseDto.java
+++ src/main/java/org/winey/server/controller/response/user/UserResponseDto.java
@@ -10,40 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserResponseDto {
-    private UserDto userDto;
-    private GoalDto goalDto;
+    private UserResponseUserDto userResponseUserDto;
+    private UserResponseGoalDto userResponseGoalDto;
 
-    public static UserResponseDto of(UserDto userDto, GoalDto goalDto) {
-        return new UserResponseDto(userDto, goalDto);
+    public static UserResponseDto of(UserResponseUserDto userResponseUserDto, UserResponseGoalDto userResponseGoalDto) {
+        return new UserResponseDto(userResponseUserDto, userResponseGoalDto);
     }
 }
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-class UserDto {
-    private Long userId;
-    private String nickname;
-    private String userLevel;
-
-    public static UserDto of(Long userId, String nickname, String userLevel) {
-        return new UserDto(userId, nickname, userLevel);
-    }
-}
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-class GoalDto {
-    private Long duringGoalAmount;
-    private Long duringGoalCount;
-    private Long targetMoney;
-    private int targetDay;
-    private boolean isOver;
-    private boolean isAttained;
-
-    public static GoalDto of(Long duringGoalAmount, Long duringGoalCount, Long targetMoney, int targetDay, boolean isOver, boolean isAttained) {
-        return new GoalDto(duringGoalAmount, duringGoalCount, targetMoney, targetDay, isOver, isAttained);
-    }
-
-}
