360ad51b7bdc329c131bccf46b6cf712dfa7a7c7
Sunny <rosa7536@naver.com>
2023-07-13 20:36:34 +0900

    #32 [Feat] requestParameter page validation

--- src/main/java/org/winey/server/controller/RecommendController.java
+++ src/main/java/org/winey/server/controller/RecommendController.java
@@ -2,11 +2,17 @@ package org.winey.server.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.winey.server.common.dto.ApiResponse;
 import org.winey.server.controller.response.recommend.RecommendListResponseDto;
+import org.winey.server.exception.Error;
 import org.winey.server.exception.Success;
 import org.winey.server.service.RecommendService;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +24,7 @@ public class RecommendController {
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<RecommendListResponseDto> getRecommend(@RequestParam int page, @RequestHeader Long userId) {
+        if (page < 1) return ApiResponse.error(Error.PAGE_REQUEST_VALIDATION_EXCEPTION, Error.PAGE_REQUEST_VALIDATION_EXCEPTION.getMessage());
         return ApiResponse.success(Success.GET_RECOMMEND_LIST_SUCCESS, recommendService.getRecommend(page, userId));
     }
 
