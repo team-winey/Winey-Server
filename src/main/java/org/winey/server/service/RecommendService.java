2fac0c58cc7897e25a9f32e6949f3dfa3d10c943
Sunny <rosa7536@naver.com>
2023-07-13 21:12:52 +0900

    [Fix] conflict 해결

--- src/main/java/org/winey/server/service/RecommendService.java
+++ src/main/java/org/winey/server/service/RecommendService.java
@@ -3,20 +3,14 @@ package org.winey.server.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.winey.server.controller.response.PageResponseDto;
 import org.winey.server.controller.response.recommend.RecommendListResponseDto;
 import org.winey.server.controller.response.recommend.RecommendResponseDto;
-import org.winey.server.controller.response.recommend.RecommendResponseUserDto;
 import org.winey.server.domain.recommend.Recommend;
-import org.winey.server.domain.user.User;
-import org.winey.server.exception.Error;
-import org.winey.server.exception.model.NotFoundException;
 import org.winey.server.infrastructure.RecommendRepository;
-import org.winey.server.infrastructure.UserRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,15 +18,10 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class RecommendService {
-    private final UserRepository userRepository;
     private final RecommendRepository recommendRepository;
 
     @Transactional
-    public RecommendListResponseDto getRecommend(int page, Long userId) {
-        User user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
-        RecommendResponseUserDto userInfo = RecommendResponseUserDto.of(user.getUserId(), user.getNickname());
-
+    public RecommendListResponseDto getRecommend(int page) {
         PageRequest pageRequest = PageRequest.of(page - 1, 50);
         Page<Recommend> recommendPage = recommendRepository.findAllByOrderByCreatedAtDesc(pageRequest);
 
@@ -43,12 +32,12 @@ public class RecommendService {
                         recommend.getRecommendId(),
                         recommend.getRecommendLink(),
                         recommend.getRecommendTitle(),
-                        recommend.getRecommendWon(),
-                        recommend.getRecommendPercent(),
+                        recommend.getRecommendSubTitle(),
+                        recommend.getRecommendPercent() != null ? String.valueOf(recommend.getRecommendPercent()) + "%" : String.valueOf(recommend.getRecommendWon() + "원"),
                         recommend.getRecommendImage(),
                         recommend.getCreatedAt()
                 )).collect(Collectors.toList());
 
-        return RecommendListResponseDto.of(userInfo, pageInfo, recommendInfos);
+        return RecommendListResponseDto.of(pageInfo, recommendInfos);
     }
 }
