1b2796c6c4eb41bfc94b953cd01a2466643b570d
soohyun <sss4920@naver.com>
2023-08-06 15:33:47 +0900

    #100 [Feat] 댓글 삭제 구현 완료 테스트 전

--- src/main/java/org/winey/server/infrastructure/CommentRepository.java
+++ src/main/java/org/winey/server/infrastructure/CommentRepository.java
@@ -5,8 +5,16 @@ import org.winey.server.domain.comment.Comment;
 import org.winey.server.domain.feed.Feed;
 import org.winey.server.domain.feed.FeedLike;
 
+import java.util.Optional;
+
 public interface CommentRepository extends Repository<Comment,Long> {
-    int countByFeed(Feed feed);
+    Long countByFeed(Feed feed);
+
+    Optional<Comment> findByCommentId(Long feedId);
 
     void save(Comment comment);
+
+    Long deleteByCommentId(Long commentId);
+
+
 }
