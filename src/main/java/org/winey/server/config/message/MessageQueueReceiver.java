f4aab696cf0e7b5dcf2c49663ecd163efd4e11a7
soohyun <sss4920@naver.com>
2023-09-10 18:02:27 +0900

    [Feat] 메시지 큐 실험 성공, fcm만 되는지 확인필요

--- src/main/java/org/winey/server/config/message/MessageQueueReceiver.java
+++ src/main/java/org/winey/server/config/message/MessageQueueReceiver.java
@@ -9,6 +9,9 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import org.winey.server.domain.notification.Notification;
 import org.winey.server.service.FcmService;
+import org.winey.server.service.message.FcmRequestDto;
+
+import java.io.*;
 
 @Component
 @AllArgsConstructor
@@ -16,23 +19,35 @@ public class MessageQueueReceiver {
 
     private final FcmService fcmService;
 
-//    @RabbitListener(bindings = @QueueBinding(
-//            exchange = @Exchange(name = "time", type = ExchangeTypes.TOPIC),
-//            value = @Queue(name = "time-second"),
-//            key = "time-first")
-//    )
-//    public void receiver(String msg){
-//        System.out.println("안녕"+msg);
-//    }
-
     @RabbitListener(bindings = @QueueBinding(
             exchange = @Exchange(name = "like", type = ExchangeTypes.TOPIC),
             value = @Queue(name = "like-notification"),
             key = "like-noti")
     )
-    public void likeReceiver(Notification likeNoti){
+    public void likeReceiver(byte[] likeNoti){
         System.out.println("좋아요 noti receiver");
-        fcmService.sendByToken(likeNoti);
+        ByteArrayInputStream bis = new ByteArrayInputStream(likeNoti);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            Object obj = in.readObject();
+            System.out.println("여기까진 오는가");
+            if (obj instanceof FcmRequestDto){
+                FcmRequestDto notification = (FcmRequestDto) obj;
+                fcmService.sendByToken(notification);
+            }
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                bis.close();
+                if (in != null){
+                    in.close();
+                }
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 
     @RabbitListener(bindings = @QueueBinding(
@@ -40,9 +55,28 @@ public class MessageQueueReceiver {
             value = @Queue(name = "comment-notification"),
             key = "comment-noti")
     )
-    public void commentReceiver(Notification commentNoti){
+    public void commentReceiver(byte[] commentNoti){
         System.out.println("댓글 noti receiver");
-        fcmService.sendByToken(commentNoti);
-
+        ByteArrayInputStream bis = new ByteArrayInputStream(commentNoti);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            Object obj = in.readObject();
+            if (obj instanceof Notification){
+                FcmRequestDto notification = (FcmRequestDto) obj;
+                fcmService.sendByToken(notification);
+            }
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                bis.close();
+                if (in != null){
+                    in.close();
+                }
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
