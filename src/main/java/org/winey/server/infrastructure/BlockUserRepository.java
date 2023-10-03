4631cc5aa1ca13831c5f6dea389cc58932aea468
Sunny <rosa7536@naver.com>
2023-10-03 15:21:34 +0900

    #175 [Feat] BlockUser 엔티티 코드 작성

---
+++ src/main/java/org/winey/server/infrastructure/BlockUserRepository.java
@@ -0,0 +1,8 @@
+package org.winey.server.infrastructure;
+
+import org.springframework.data.repository.Repository;
+import org.winey.server.domain.block.BlockUser;
+
+public interface BlockUserRepository extends Repository<BlockUser, Long> {
+
+}
---
+++ src/main/java/org/winey/server/service/BlockService.java
@@ -0,0 +1,10 @@
+package org.winey.server.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BlockService {
+
+}
