436117e9d04dfba98c7486cc7f69611f4cd592bd
sunny <rosa7536@naver.com>
2023-10-03 16:40:18 +0900

    #175 [Feat] 차단 목록 조회시 fetch join

--- src/main/java/org/winey/server/infrastructure/BlockUserRepository.java
+++ src/main/java/org/winey/server/infrastructure/BlockUserRepository.java
@@ -1,6 +1,7 @@
 package org.winey.server.infrastructure;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.winey.server.domain.block.BlockUser;
 import org.winey.server.domain.user.User;
@@ -9,5 +10,6 @@ public interface BlockUserRepository extends Repository<BlockUser, Long> {
 
     BlockUser save(BlockUser blockUser);
 
+    @Query("select bu from BlockUser bu join fetch bu.responseUser where bu.requestUser = :requestUser")
     List<BlockUser> findByRequestUser(User requestUser);
 }
