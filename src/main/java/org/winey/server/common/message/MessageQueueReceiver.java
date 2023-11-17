c01657804671b1c708039e708375650315c4cf0c
Seonheui Jeon <88873302+funnysunny08@users.noreply.github.com>
2023-11-17 21:39:54 +0900

    feat: FCM 배포 (#190)
    * [Refactor] 알림생성로직 관련 중복 코드 개선
    
    * [Feat] fcm 관련 로직 구현 (#187)
    
    * [Feat] fcm service 구현하기
    
    * #159 [Feat] createMessage queue
    
    * [Feat] 좋아요, 댓글 생성시 알림 발송
    
    * [Feat] 메시지 큐 실험 성공, fcm만 되는지 확인필요
    
    * [Feat] 푸시sender transactional 제거
    
    * [Feat] fcm토큰 준비
    
    * [Feat] rabbitMQ server ec2연결 및 token response 요구사항 반영수정
    
    * [Feat] updateFcmtoken 구현 및 notification success notFound -> no content수정
    
    * [Merge] merge confict 해결
    
    * [Fix] 닉네임 수정 로직이랑 줄을 헷갈려서 버그 수정
    
    * [FIX] merge conflict
    
    * [FIX] stash 결과 적용
    
    * [Feat] 동의여부 기능 추가 및 fcm 메시지 전송 변경
    
    * [Feat] 댓글, 좋아요 알림로직에 동의 여부 수정 및 예외처리
    
    * [Fix] notificationresponse Id 수정
    
    * [Fix] logic 수정
    
    * Update cd.yml
    
    * Update gradle.yml
    
    ---------
    
    Co-authored-by: soohyun <sss4920@naver.com>
    Co-authored-by: soohyun <49307946+sss4920@users.noreply.github.com>

---
+++ src/main/java/org/winey/server/common/message/MessageQueueReceiver.java
@@ -0,0 +1,82 @@
+package org.winey.server.common.message;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.winey.server.domain.notification.Notification;
+import org.winey.server.service.FcmService;
+import org.winey.server.service.message.FcmRequestDto;
+
+import java.io.*;
+
+@Component
+@AllArgsConstructor
+public class MessageQueueReceiver {
+
+    private final FcmService fcmService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(name = "like", type = ExchangeTypes.TOPIC),
+            value = @Queue(name = "like-notification"),
+            key = "like-noti")
+    )
+    public void likeReceiver(byte[] likeNoti){
+        System.out.println("좋아요 noti receiver");
+        ByteArrayInputStream bis = new ByteArrayInputStream(likeNoti);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            Object obj = in.readObject();
+            System.out.println("여기까진 오는가");
+            if (obj instanceof FcmRequestDto){
+                FcmRequestDto notification = (FcmRequestDto) obj;
+                fcmService.sendByToken(notification);
+            }
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                bis.close();
+                if (in != null){
+                    in.close();
+                }
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(name = "comment", type = ExchangeTypes.TOPIC),
+            value = @Queue(name = "comment-notification"),
+            key = "comment-noti")
+    )
+    public void commentReceiver(byte[] commentNoti){
+        System.out.println("댓글 noti receiver");
+        ByteArrayInputStream bis = new ByteArrayInputStream(commentNoti);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            Object obj = in.readObject();
+            if (obj instanceof Notification){
+                FcmRequestDto notification = (FcmRequestDto) obj;
+                fcmService.sendByToken(notification);
+            }
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                bis.close();
+                if (in != null){
+                    in.close();
+                }
+            }catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+}
