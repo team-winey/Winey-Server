8bb97eedf49f13b9a2f4a07ab1963bca80c5226f
Seonheui Jeon <88873302+funnysunny08@users.noreply.github.com>
2023-11-17 21:39:54 +0900

    feat: FCM 배포 (#190)
    * [Refactor] 알림생성로직 관련 중복 코드 개선
    
    * [Feat] fcm 관련 로직 구현 (#187)
    
    * [Feat] fcm service 구현하기
    
    * #159 [Feat] createMessage queue
    
    * [Feat] 좋아요, 댓글 생성시 알림 발송
    
    * [Feat] 메시지 큐 실험 성공, fcm만 되는지 확인필요
    
    * [Feat] 푸시sender transactional 제거
    
    * [Feat] fcm토큰 준비
    
    * [Feat] rabbitMQ server ec2연결 및 token response 요구사항 반영수정
    
    * [Feat] updateFcmtoken 구현 및 notification success notFound -> no content수정
    
    * [Merge] merge confict 해결
    
    * [Fix] 닉네임 수정 로직이랑 줄을 헷갈려서 버그 수정
    
    * [FIX] merge conflict
    
    * [FIX] stash 결과 적용
    
    * [Feat] 동의여부 기능 추가 및 fcm 메시지 전송 변경
    
    * [Feat] 댓글, 좋아요 알림로직에 동의 여부 수정 및 예외처리
    
    * [Fix] notificationresponse Id 수정
    
    * [Fix] logic 수정
    
    * Update cd.yml
    
    * Update gradle.yml
    
    ---------
    
    Co-authored-by: soohyun <sss4920@naver.com>
    Co-authored-by: soohyun <49307946+sss4920@users.noreply.github.com>

--- src/main/java/org/winey/server/service/CommentService.java
+++ src/main/java/org/winey/server/service/CommentService.java
@@ -1,13 +1,17 @@
 package org.winey.server.service;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.winey.server.common.message.MessageQueueSender;
 import org.winey.server.controller.response.comment.CommentResponseDto;
 import org.winey.server.controller.response.comment.DeleteCommentResponseDto;
 import org.winey.server.domain.comment.Comment;
 import org.winey.server.domain.notification.NotiType;
 import org.winey.server.domain.notification.Notification;
+import org.winey.server.exception.model.BadRequestException;
+import org.winey.server.exception.model.CustomException;
 import org.winey.server.exception.model.UnauthorizedException;
 import org.winey.server.exception.model.UnprocessableEntityException;
 import org.winey.server.infrastructure.CommentRepository;
@@ -18,68 +22,89 @@ import org.winey.server.exception.model.NotFoundException;
 import org.winey.server.infrastructure.FeedRepository;
 import org.winey.server.infrastructure.NotiRepository;
 import org.winey.server.infrastructure.UserRepository;
-
+import org.winey.server.service.message.FcmRequestDto;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
-    private final UserRepository userRepository;
-    private final FeedRepository feedRepository;
-    private final CommentRepository commentRepository;
-    private final NotiRepository notiRepository;
+	private final UserRepository userRepository;
+	private final FeedRepository feedRepository;
+	private final CommentRepository commentRepository;
+	private final NotiRepository notiRepository;
 
-    @Transactional
-    public CommentResponseDto createComment(Long userId, Long feedId, String content) {
-        User user = userRepository.findByUserId(userId)
-                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
-        Feed feed = feedRepository.findByFeedId(feedId)
-                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_FEED_EXCEPTION, Error.NOT_FOUND_FEED_EXCEPTION.getMessage()));
+	private final MessageQueueSender messageQueueSender;
 
+	@Transactional
+	public CommentResponseDto createComment(Long userId, Long feedId, String content) {
+		User user = userRepository.findByUserId(userId)
+			.orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION,
+				Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+		Feed feed = feedRepository.findByFeedId(feedId)
+			.orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_FEED_EXCEPTION,
+				Error.NOT_FOUND_FEED_EXCEPTION.getMessage()));
 
-        Comment comment = Comment.builder()
-                .user(user)
-                .content(content)
-                .feed(feed)
-                .build();
-        commentRepository.save(comment);
+		Comment comment = Comment.builder()
+			.user(user)
+			.content(content)
+			.feed(feed)
+			.build();
+		commentRepository.save(comment);
 
-        if (user.getUserId() != feed.getUser().getUserId()) {
-            Notification notification = Notification.builder() //누군가가 내 피드에 댓글을 달았어요~
-                    .notiReciver(feed.getUser())
-                    .notiType(NotiType.COMMENTNOTI)
-                    .notiMessage(comment.getUser().getNickname() + NotiType.COMMENTNOTI.getType())
-                    .isChecked(false)
-                    .build();
-            notification.updateLinkId(feedId);
-            notification.updateResponseId(comment.getCommentId());
-            notification.updateRequestUserId(userId);
-            notiRepository.save(notification);
-        }
-        return CommentResponseDto.of(comment.getCommentId(), userId, user.getNickname(), comment.getContent(),user.getUserLevel().getLevelNumber(),comment.getCreatedAt());
-    }
+		if (user.getUserId() != feed.getUser().getUserId()) {
+			createNotificationInComment(feed, comment, user); //알림 생성 후 message queue에 task 등록
+		}
+		return CommentResponseDto.of(comment.getCommentId(), userId, user.getNickname(), comment.getContent(),
+			user.getUserLevel().getLevelNumber(), comment.getCreatedAt());
+	}
 
-    @Transactional
-    public DeleteCommentResponseDto deleteComment(Long userId, Long commentId){
-        User user = userRepository.findByUserId(userId)
-                .orElseThrow(()-> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
-        Comment wantDeleteComment = commentRepository.findByCommentId(commentId)
-                .orElseThrow(()-> new NotFoundException(Error.NOT_FOUND_COMMENT_EXCEPTION, Error.NOT_FOUND_COMMENT_EXCEPTION.getMessage()));
-        Feed commentFeed = feedRepository.findByFeedId(wantDeleteComment.getFeed().getFeedId())
-                .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_FEED_EXCEPTION, Error.NOT_FOUND_FEED_EXCEPTION.getMessage()));
+	@Transactional
+	public DeleteCommentResponseDto deleteComment(Long userId, Long commentId) {
+		User user = userRepository.findByUserId(userId)
+			.orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION,
+				Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+		Comment wantDeleteComment = commentRepository.findByCommentId(commentId)
+			.orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_COMMENT_EXCEPTION,
+				Error.NOT_FOUND_COMMENT_EXCEPTION.getMessage()));
+		Feed commentFeed = feedRepository.findByFeedId(wantDeleteComment.getFeed().getFeedId())
+			.orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_FEED_EXCEPTION,
+				Error.NOT_FOUND_FEED_EXCEPTION.getMessage()));
 
-        if (user.getUserId() != wantDeleteComment.getUser().getUserId() && user.getUserId() != commentFeed.getUser().getUserId() ){ //만약 현재 유저가 피드 주인도 아니고, 댓글 주인도 아니면?
-                throw new UnauthorizedException(Error.DELETE_UNAUTHORIZED, Error.DELETE_UNAUTHORIZED.getMessage()); //지울 수 있는 권한이 없다.
-        }
+		if (user.getUserId() != wantDeleteComment.getUser().getUserId() && user.getUserId() != commentFeed.getUser()
+			.getUserId()) { //만약 현재 유저가 피드 주인도 아니고, 댓글 주인도 아니면?
+			throw new UnauthorizedException(Error.DELETE_UNAUTHORIZED,
+				Error.DELETE_UNAUTHORIZED.getMessage()); //지울 수 있는 권한이 없다.
+		}
 
-        // 관련 알림 삭제
-        notiRepository.deleteByNotiTypeAndResponseId(NotiType.COMMENTNOTI, wantDeleteComment.getCommentId());
+		// 관련 알림 삭제
+		notiRepository.deleteByNotiTypeAndResponseId(NotiType.COMMENTNOTI, wantDeleteComment.getCommentId());
 
-        Long res = commentRepository.deleteByCommentId(commentId);
-        if (res != 1){
-            throw new UnprocessableEntityException(Error.UNPROCESSABLE_ENTITY_DELETE_EXCEPTION,Error.UNPROCESSABLE_ENTITY_DELETE_EXCEPTION.getMessage());
-        }
-        return DeleteCommentResponseDto.of(commentId);
-    }
+		Long res = commentRepository.deleteByCommentId(commentId);
+		if (res != 1) {
+			throw new UnprocessableEntityException(Error.UNPROCESSABLE_ENTITY_DELETE_EXCEPTION,
+				Error.UNPROCESSABLE_ENTITY_DELETE_EXCEPTION.getMessage());
+		}
+		return DeleteCommentResponseDto.of(commentId);
+	}
 
+	private void createNotificationInComment(Feed feed, Comment comment, User user) {
+		Notification notification = Notification.builder() //누군가가 내 피드에 댓글을 달았어요~
+			.notiReciver(feed.getUser())
+			.notiType(NotiType.COMMENTNOTI)
+			.notiMessage(comment.getUser().getNickname() + NotiType.COMMENTNOTI.getType())
+			.isChecked(false)
+			.build();
+		notification.updateLinkId(feed.getFeedId());
+		notification.updateResponseId(comment.getCommentId());
+		notification.updateRequestUserId(user.getUserId());
+		notiRepository.save(notification);
+		if (feed.getUser().getFcmIsAllowed() && !notification.getNotiReceiver().getFcmToken().isEmpty()) { //푸시알림에 동의했을 경우.
+			messageQueueSender.pushSender(
+				FcmRequestDto.of(
+                    notification.getNotiMessage(),
+                    notification.getNotiReceiver().getFcmToken(),
+					notification.getNotiType(),
+					feed.getFeedId()));
+		}
+	}
 
 }
--- src/main/java/org/winey/server/service/UserService.java
+++ src/main/java/org/winey/server/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.joda.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.winey.server.controller.request.UpdateFcmTokenDto;
 import org.winey.server.controller.request.UpdateUserNicknameDto;
 import org.winey.server.controller.response.user.UserResponseDto;
 import org.winey.server.controller.response.user.UserResponseGoalDto;
@@ -13,6 +14,7 @@ import org.winey.server.domain.notification.NotiType;
 import org.winey.server.domain.notification.Notification;
 import org.winey.server.domain.user.User;
 import org.winey.server.exception.Error;
+import org.winey.server.exception.model.BadRequestException;
 import org.winey.server.exception.model.NotFoundException;
 import org.winey.server.infrastructure.GoalRepository;
 import org.winey.server.infrastructure.NotiRepository;
@@ -29,14 +31,13 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final GoalRepository goalRepository;
-    private final NotiRepository notiRepository;
 
     @Transactional(readOnly = true)
     public UserResponseDto getUser(Long userId) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
 
-        UserResponseUserDto userDto = UserResponseUserDto.of(user.getUserId(), user.getNickname(), user.getUserLevel().getName());
+        UserResponseUserDto userDto = UserResponseUserDto.of(user.getUserId(), user.getNickname(), user.getUserLevel().getName(),user.getFcmIsAllowed());
 
         List<Goal> goalList = goalRepository.findByUserOrderByCreatedAtDesc(user);
 
@@ -57,17 +58,26 @@ public class UserService {
     public void updateNickname(Long userId, UpdateUserNicknameDto requestDto) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
-//        List<Notification> notifications = notiRepository.findByRequestUserId(userId);
-//        if (!notifications.isEmpty()) {
-//            notifications.forEach(notification -> {
-//                if (notification.getNotiType() == NotiType.COMMENTNOTI) {
-//                    notification.updateNotiMessage(requestDto.getNickname() + NotiType.COMMENTNOTI.getType());
-//                } else {
-//                    notification.updateNotiMessage(requestDto.getNickname() + NotiType.LIKENOTI.getType());
-//                }
-//            });
-//        }
         user.updateNickname(requestDto.getNickname());
+    }
+    @Transactional
+    public void updateFcmToken(Long userId, UpdateFcmTokenDto updateFcmTokenDto){
+        User user = userRepository.findByUserId(userId)
+            .orElseThrow(() -> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+        user.updateFcmToken(updateFcmTokenDto.getToken());
+    }
+
+    //푸시알림 동의 여부 수정 api
+    @Transactional
+    public Boolean allowedPushNotification(Long userId, Boolean fcmIsAllowed){
+        User user = userRepository.findByUserId(userId)
+            .orElseThrow(()-> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage()));
+        if (fcmIsAllowed == user.getFcmIsAllowed()) {   //같은 경우면 에러가 날 수 있으니 에러 띄움.
+            throw new BadRequestException(Error.REQUEST_VALIDATION_EXCEPTION,
+                Error.REQUEST_VALIDATION_EXCEPTION.getMessage());
+        }
+        user.updateFcmIsAllowed(fcmIsAllowed);
+        return fcmIsAllowed;
     }
 
     public Boolean checkNicknameDuplicate(String nickname) {
