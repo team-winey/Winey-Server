1d72e6141bfd88a27d04de889bd558ff780d2dce
Sunny <rosa7536@naver.com>
2023-12-11 21:52:27 +0900

    [Refactor] 불필요한 공백 제거

--- src/main/java/org/winey/server/controller/RecommendController.java
+++ src/main/java/org/winey/server/controller/RecommendController.java
@@ -4,17 +4,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.winey.server.common.dto.ApiResponse;
 import org.winey.server.controller.response.recommend.RecommendListResponseDto;
 import org.winey.server.exception.Error;
 import org.winey.server.exception.Success;
 import org.winey.server.service.RecommendService;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,5 +31,4 @@ public class RecommendController {
             return ApiResponse.error(Error.PAGE_REQUEST_VALIDATION_EXCEPTION, Error.PAGE_REQUEST_VALIDATION_EXCEPTION.getMessage());
         return ApiResponse.success(Success.GET_RECOMMEND_LIST_SUCCESS, recommendService.getRecommend(page));
     }
-
 }
