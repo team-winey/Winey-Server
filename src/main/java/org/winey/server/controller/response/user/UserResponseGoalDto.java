e6de798f9dfe59e1d06f05fef4301791223852ec
sunny <rosa7536@naver.com>
2024-01-04 22:54:42 +0900

    [Feat] 마이페이지에서 현재 진행중인 목표 반환 로직 수정

--- src/main/java/org/winey/server/controller/response/user/UserResponseGoalDto.java
+++ src/main/java/org/winey/server/controller/response/user/UserResponseGoalDto.java
@@ -1,25 +1,31 @@
 package org.winey.server.controller.response.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.winey.server.domain.goal.GoalType;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserResponseGoalDto {
+    private GoalType goalType;
     private Long duringGoalAmount;
     private Long duringGoalCount;
-    private Long targetMoney;
-    private int targetDay;
-    private int dDay;
-    private Boolean isOver;
     private Boolean isAttained;
 
-    public static UserResponseGoalDto of(Long duringGoalAmount, Long duringGoalCount, Long targetMoney, int targetDay, int dDay, boolean isOver, boolean isAttained) {
-        return new UserResponseGoalDto(duringGoalAmount, duringGoalCount, targetMoney, targetDay, dDay, isOver, isAttained);
+    public static UserResponseGoalDto of(
+        GoalType goalType,
+        Long duringGoalAmount,
+        Long duringGoalCount,
+        boolean isAttained)
+    {
+        return new UserResponseGoalDto(
+            goalType,
+            duringGoalAmount,
+            duringGoalCount,
+            isAttained
+        );
     }
-
 }
