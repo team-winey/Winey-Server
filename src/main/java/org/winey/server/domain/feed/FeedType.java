5b47ef3b6ee5b73d6ac9acd11ab07ed5d574b325
김응관 <kwhn9486@naver.com>
2024-01-05 23:15:09 +0900

    클라이언트로부터 받은 FeedType이 유효한 타입인지 검증하는 메서드 작성

--- src/main/java/org/winey/server/domain/feed/FeedType.java
+++ src/main/java/org/winey/server/domain/feed/FeedType.java
@@ -6,5 +6,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FeedType {
-    SAVE, CONSUME
+    SAVE("SAVE"),
+    CONSUME("CONSUME");
+
+    private final String stringVal;
+
+    public static boolean isValidFeedType(String type) {
+        for (FeedType feed : FeedType.values()) {
+            if (feed.getStringVal() == type) return true;
+        }
+        return false;
+    }
 }
