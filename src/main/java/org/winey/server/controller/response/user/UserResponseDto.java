e69f26a6f9c246cace6f8f7dda5647d85af2eeb5
김응관 <kwhn9486@naver.com>
2024-02-04 22:41:15 +0900

    [Fix] 마이페이지 API 구조 변경

--- src/main/java/org/winey/server/controller/response/user/UserResponseDto.java
+++ src/main/java/org/winey/server/controller/response/user/UserResponseDto.java
@@ -11,19 +11,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserResponseDto {
 
-    private Long userId;
-    private String nickname;
-    private String userLevel;
-    private Boolean fcmIsAllowed;
-    private Long accumulatedAmount;
-    private Long amountSavedHundredDays;
-    private Long amountSavedTwoWeeks;
-    private Long amountSpentTwoWeeks;
+    private UserData userData;
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class UserData {
+        private Long userId;
+        private String nickname;
+        private String userLevel;
+        private Boolean fcmIsAllowed;
+        private Long accumulatedAmount;
+        private Long amountSavedHundredDays;
+        private Long amountSavedTwoWeeks;
+        private Long amountSpentTwoWeeks;
+    }
 
     public static UserResponseDto of(Long userId, String nickname, String userLevel,
         Boolean fcmIsAllowed, Long accumulatedAmount, Long amountSavedHundredDays, Long amountSavedTwoWeeks,
         Long amountSpentTwoWeeks) {
-        return new UserResponseDto(userId, nickname, userLevel, fcmIsAllowed, accumulatedAmount,
-            amountSavedHundredDays, amountSavedTwoWeeks, amountSpentTwoWeeks);
+        UserData userData = new UserData(userId, nickname, userLevel, fcmIsAllowed, accumulatedAmount,
+                amountSavedHundredDays, amountSavedTwoWeeks, amountSpentTwoWeeks);
+        return new UserResponseDto(userData);
     }
 }
