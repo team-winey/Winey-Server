cf47b9ab6a368d5c826a4b05091ffdaa0aa3dabd
soohyun <sss4920@naver.com>
2025-02-06 16:18:10 +0900

    hotfix: profile추가

--- src/main/java/org/winey/server/config/ssh/SshTunnelingInitializer.java
+++ src/main/java/org/winey/server/config/ssh/SshTunnelingInitializer.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 import static java.lang.System.exit;
 
 @Slf4j
-@Profile("local")
+@Profile({"local","set1","set2"})
 @Component
 @ConfigurationProperties(prefix = "ssh")
 @Validated
