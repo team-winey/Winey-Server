02f886401b42a62d019351678d465c6eb27df376
soohyun <sss4920@naver.com>
2025-02-06 16:06:19 +0900

    hotfix: ssh터널링 구성

---
+++ src/main/java/org/winey/server/config/ssh/SshDataSourceConfig.java
@@ -0,0 +1,37 @@
+package org.winey.server.config.ssh;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Profile("local")
+@Configuration
+@RequiredArgsConstructor
+public class SshDataSourceConfig {
+
+    private final SshTunnelingInitializer initializer;
+
+    @Bean("dataSource")
+    @Primary
+    public DataSource dataSource(DataSourceProperties properties) {
+
+        Integer forwardedPort = initializer.buildSshConnection();  // ssh 연결 및 터널링 설정
+        String url = properties.getUrl().replace("[forwardedPort]", Integer.toString(forwardedPort));
+        log.info(url);
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(properties.getUsername())
+                .password(properties.getPassword())
+                .driverClassName(properties.getDriverClassName())
+                .build();
+    }
+
+}
---
+++ src/main/java/org/winey/server/config/ssh/SshTunnelingInitializer.java
@@ -0,0 +1,83 @@
+package org.winey.server.config.ssh;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.annotation.PreDestroy;
+import javax.validation.constraints.NotNull;
+import java.util.Properties;
+
+import static java.lang.System.exit;
+
+@Slf4j
+@Profile("local")
+@Component
+@ConfigurationProperties(prefix = "ssh")
+@Validated
+@Setter
+public class SshTunnelingInitializer {
+
+    @NotNull
+    private String remoteJumpHost;
+    @NotNull
+    private String user;
+    @NotNull
+    private int sshPort;
+    @NotNull
+    private String privateKey;
+    @NotNull
+    private String databaseUrl;
+    @NotNull
+    private int databasePort;
+
+    private Session session;
+
+    @PreDestroy
+    public void closeSSH() {
+        if (session.isConnected())
+            session.disconnect();
+    }
+
+    public Integer buildSshConnection() {
+
+        Integer forwardedPort = null;
+
+        try {
+            log.info("{}@{}:{}:{} with privateKey",user, remoteJumpHost, sshPort, databasePort);
+
+            log.info("start ssh tunneling..");
+            JSch jSch = new JSch();
+
+            log.info("creating ssh session");
+            jSch.addIdentity(privateKey);  // 개인키
+            session = jSch.getSession(user, remoteJumpHost, sshPort);  // 세션 설정
+            Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            log.info("complete creating ssh session");
+
+            log.info("start connecting ssh connection");
+            session.connect();  // ssh 연결
+            log.info("success connecting ssh connection ");
+
+            // 로컬pc의 남는 포트 하나와 원격 접속한 pc의 db포트 연결
+            log.info("start forwarding");
+            forwardedPort = session.setPortForwardingL(0, databaseUrl, databasePort);
+            log.info("successfully connected to database");
+
+        } catch (Exception e){
+            log.error("fail to make ssh tunneling");
+            this.closeSSH();
+            e.printStackTrace();
+            exit(1);
+        }
+
+        return forwardedPort;
+    }
+}
